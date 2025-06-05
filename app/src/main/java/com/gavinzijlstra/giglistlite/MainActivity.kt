2a2733ce0a4314d0894991b601d337d6b6417f55
Gavin Zijlstra <info@gavinzijlstra.nl>
2025-06-06 00:13:30 +0200

    tweede opzet add button

--- app/src/main/java/com/gavinzijlstra/giglistlite/MainActivity.kt
+++ app/src/main/java/com/gavinzijlstra/giglistlite/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.gavinzijlstra.giglistlite
 
 import android.app.AlertDialog
+import android.content.res.ColorStateList
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -40,23 +41,23 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<List<Gig>>, response: Response<List<Gig>>) {
                 if (response.isSuccessful) {
                     allGigs = response.body() ?: emptyList()
-                    Log.d("MainActivity", "Gigs ontvangen: ${allGigs.size}")
+                    Log.d("GigList-MainActivity", "Gigs ontvangen: ${allGigs.size}")
                     gigAdapter = GigAdapter(allGigs)
                     recyclerView.adapter = gigAdapter
                 } else {
-                    Log.e("MainActivity", "Response fout: ${response.code()} - ${response.message()}")
+                    Log.e("GigList-MainActivity", "Response fout: ${response.code()} - ${response.message()}")
                 }
             }
 
             override fun onFailure(call: Call<List<Gig>>, t: Throwable) {
-                Log.e("MainActivity", "Fout bij ophalen data", t)
+                Log.e("GigList-MainActivity", "Fout bij ophalen data", t)
                 Toast.makeText(this@MainActivity, "Fout bij ophalen data", Toast.LENGTH_SHORT).show()
             }
         })
 
         searchBar.addTextChangedListener {
             val query = it.toString()
-            Log.d("MainActivity", "Search for: " + query)
+            Log.d("GigList-MainActivity", "Search for: " + query)
             val filteredList = allGigs.filter { gig ->
                 gig.band.contains(query, ignoreCase = true)
             }
@@ -65,26 +66,40 @@ class MainActivity : AppCompatActivity() {
 
         addGigFab.setOnClickListener {
             val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_add_gig, null)
-            val dialog = AlertDialog.Builder(this)
-                .setTitle("Nieuwe Gig toevoegen")
+            val dialogBuilder = AlertDialog.Builder(this)
                 .setView(dialogView)
-                .setPositiveButton("Opslaan") { _, _ ->
+                .setTitle("Nieuwe Gig toevoegen")
+                .setPositiveButton("Opslaan", null)  // Listener later instellen
+                .setNegativeButton("Annuleren", null)
+
+            val dialog = dialogBuilder.create()
+            dialog.setOnShowListener {
+                dialog.getButton(AlertDialog.BUTTON_NEGATIVE)?.setTextColor(Color.BLACK)
+                val btnSave = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                btnSave.setTextColor(Color.BLACK)
+                btnSave.setOnClickListener {
                     val artist = dialogView.findViewById<EditText>(R.id.artistInput).text.toString()
                     val date = dialogView.findViewById<EditText>(R.id.dateInput).text.toString()
                     val venue = dialogView.findViewById<EditText>(R.id.venueInput).text.toString()
+                    val zaal = dialogView.findViewById<EditText>(R.id.zaalInput).text.toString()
+                    val opmerking = dialogView.findViewById<EditText>(R.id.opmerkingInput).text.toString()
+
                     // Maak nieuwe Gig en voeg toe aan lijst
 
-                    Log.d("Created Gig", "artist: ${artist},date: ${date}, venue: ${venue} ")
-
+                    Log.d("GigList-MainActivity", "artist: ${artist},date: ${date}, venue: ${venue} ")
+                    // Validatie en toevoegen aan je lijst/adapter
+                    if (artist.isNotBlank() && venue.isNotBlank() && date.isNotBlank()) {
+                        //val newGig = Gig(artist = artist, venue = venue, date = date, details = "")
+                        //allGigs.add(newGig)
+                        //gigAdapter.updateList(allGigs)
+                        dialog.dismiss()
+                    } else {
+                        Toast.makeText(this, "De velden band, zaal en datum zijn.", Toast.LENGTH_SHORT)
+                            .show()
+                    }
                 }
-                .setNegativeButton("Annuleren", null)
-                .create()
-            dialog.setOnShowListener {
-                dialog.getButton(AlertDialog.BUTTON_POSITIVE)?.setTextColor(Color.BLACK)
-                dialog.getButton(AlertDialog.BUTTON_NEGATIVE)?.setTextColor(Color.BLACK)
             }
             dialog.show()
-
         }
     }
 }
